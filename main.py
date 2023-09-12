cb927db6d7a125d125a457b9d52a1db2d11a17d4
Gagandeep Singh <130401523+gdssingh7@users.noreply.github.com>
2023-09-12 12:46:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import streamlit as st
 import pandas as pd
 import random
+import time
+import base64
+
 def main():
     st.title("Prize Generator App")
+    
+    st.markdown("""
+        <style>
+            body {
+                background-color: #E6E6FA;
+            }
+            .stButton>button {
+                background-color: #4CAF50;
+                color: white;
+            }
+        </style>
+        """, unsafe_allow_html=True)
 
     if 'data' not in st.session_state:
         st.session_state.data = None
@@ -11,23 +26,22 @@ def main():
 
     if uploaded_file:
         st.session_state.data = pd.read_excel(uploaded_file)
-
-    if st.session_state.data is not None:
         st.write(st.session_state.data)
 
+    if st.session_state.data is not None:
         ticket_col = st.selectbox("Select the column for number of tickets bought:", list(st.session_state.data.columns))
         city_col = st.selectbox("Select the column for cities:", list(st.session_state.data.columns))
 
         if ticket_col and 'Assigned Tickets' not in st.session_state.data.columns:
-            st.session_state.data = assign_ticket_numbers(st.session_state.data, ticket_col)
-            st.write(st.session_state.data)
+            if st.button("Assign Ticket Numbers"):
+                st.session_state.data = assign_ticket_numbers(st.session_state.data, ticket_col)
+                st.write(st.session_state.data)
 
         if 'Assigned Tickets' in st.session_state.data.columns:
             num_winners = st.number_input("Enter the number of winners:", min_value=1, max_value=len(st.session_state.data))
             if st.button("Select Winners"):
                 winners = select_random_winners(st.session_state.data, num_winners)
-                st.write("Winners:")
-                st.write(winners)
+                st.markdown(download_link(winners, "winners.csv", "Download Winners"), unsafe_allow_html=True)
 
         if city_col:
             special_city = st.selectbox("Select a city for special prizes:", list(st.session_state.data[city_col].unique()))
@@ -36,23 +50,19 @@ def main():
                 num_special_winners = st.number_input("Enter the number of special winners:", min_value=1, max_value=max_special_winners)
                 if st.button("Select Special Winners"):
                     special_winners = select_special_winners(st.session_state.data, city_col, special_city, num_special_winners)
-                    st.write(f"Special Winners from {special_city}:")
-                    st.write(special_winners)
-            elif 'Assigned Tickets' not in st.session_state.data.columns:
-                st.warning("Please assign ticket numbers before selecting special winners.")
+                    st.markdown(download_link(special_winners, "special_winners.csv", "Download Special Winners"), unsafe_allow_html=True)
 
 def assign_ticket_numbers(data, ticket_col):
     ticket_counter = 1
     ticket_numbers = []
 
     for _, row in data.iterrows():
-        num_tickets = int(row[ticket_col])  # Ensure it's an integer
+        num_tickets = int(row[ticket_col])
         tickets_for_person = list(range(ticket_counter, ticket_counter + num_tickets))
         ticket_numbers.append(tickets_for_person)
         ticket_counter += num_tickets
 
     data['Assigned Tickets'] = ticket_numbers
-
     return data
 
 def select_random_winners(data, p):
@@ -62,7 +72,11 @@ def select_random_winners(data, p):
     winners = []
     for ticket in winning_tickets:
         winner_info = data[data['Assigned Tickets'].apply(lambda x: ticket in x)].iloc[0]
+        winner_info['Winning Ticket'] = ticket
         winners.append(winner_info)
+        st.write(f"Winner Ticket: {ticket}")
+        st.write(winner_info)
+        time.sleep(1)
 
     return pd.DataFrame(winners)
 
@@ -79,9 +93,20 @@ def select_special_winners(data, city_col, city, k):
     city_winners = []
     for ticket in winning_city_tickets:
         winner_info = city_data[city_data['Assigned Tickets'].apply(lambda x: ticket in x)].iloc[0]
+        winner_info['Winning Ticket'] = ticket
         city_winners.append(winner_info)
+        st.write(f"Special Winner Ticket from {city}: {ticket}")
+        st.write(winner_info)
+        time.sleep(1)
 
     return pd.DataFrame(city_winners)
+
+def download_link(object_to_download, download_filename, download_link_text):
+    if isinstance(object_to_download, pd.DataFrame):
+        object_to_download = object_to_download.to_csv(index=False)
+
+    b64 = base64.b64encode(object_to_download.encode()).decode()
+    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
 
 if __name__ == "__main__":
     main()
