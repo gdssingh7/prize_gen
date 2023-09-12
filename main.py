d6d03fc90382d6429b5177dce66c18dc1b7aab77
Gagandeep Singh <130401523+gdssingh7@users.noreply.github.com>
2023-09-12 11:15:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import random
 
+import streamlit as st
+import pandas as pd
+import random
+
 def main():
     st.title("Prize Generator App")
 
@@ -37,6 +41,55 @@ def main():
                 special_winners = select_special_winners(data, city_col, special_city, num_special_winners)
                 st.write(f"Special Winners from {special_city}:")
                 st.write(special_winners)
+
+def assign_ticket_numbers(data, ticket_col):
+    ticket_counter = 1
+    ticket_numbers = []
+
+    for _, row in data.iterrows():
+        num_tickets = row[ticket_col]
+        tickets_for_person = list(range(ticket_counter, ticket_counter + num_tickets))
+        ticket_numbers.append(tickets_for_person)
+        ticket_counter += num_tickets
+
+    data['Assigned Tickets'] = ticket_numbers
+
+    return data
+
+import random
+
+def select_random_winners(data, p):
+    all_tickets = [ticket for sublist in data['Assigned Tickets'] for ticket in sublist]
+    winning_tickets = random.sample(all_tickets, p)
+
+    winners = []
+    for ticket in winning_tickets:
+        winner_info = data[data['Assigned Tickets'].apply(lambda x: ticket in x)].iloc[0]
+        winners.append(winner_info)
+
+    return pd.DataFrame(winners)
+
+
+def select_special_winners(data, city_col, city, k):
+    city_data = data[data[city_col] == city]
+    all_city_tickets = [ticket for sublist in city_data['Assigned Tickets'] for ticket in sublist]
+    
+    if k > len(all_city_tickets):
+        st.warning("The number of special prizes exceeds the total number of tickets from the selected city. Adjusting to maximum available.")
+        k = len(all_city_tickets)
+    
+    winning_city_tickets = random.sample(all_city_tickets, k)
+    
+    city_winners = []
+    for ticket in winning_city_tickets:
+        winner_info = city_data[city_data['Assigned Tickets'].apply(lambda x: ticket in x)].iloc[0]
+        city_winners.append(winner_info)
+
+    return pd.DataFrame(city_winners)
+
+
+if __name__ == "__main__":
+    main()
 
 def assign_ticket_numbers(data, ticket_col):
     ticket_counter = 1
